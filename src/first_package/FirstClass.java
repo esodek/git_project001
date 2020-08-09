7573b5d0bfe104083d8e8d6b49dd21212620f00c
Ismail Sodek <esodek@gmail.com>
2020-08-09 17:49:51 +0200

    our second commit from local repository

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -1,5 +1,20 @@
 package first_package;
 
 public class FirstClass {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("our first commit from local repo");
+		System.out.println("our second commit from local repository");
+		
+		
+		
+		
+		
+		
+	}
 
 }
